f9890727bdaa20938feaa9847dcd6c5eb4dcc8aa
JoaoPedroFortes <jpedrofortes@outlook.com>
2021-08-08 20:12:55 -0400

    [correcao] get user by id

--- pages/api/user/get-by-id.js
+++ pages/api/user/get-by-id.js
@@ -4,21 +4,31 @@ import { loadFirebase } from '../../../lib/db.js'
 
 async function getUser(req, res) {
     try {
+        console.log("oii")
         if (req.method === 'POST') {
             const userId = req.body.userId;
-    
             const result = await (getUserById(userId));
-            console.log("res: ", await result)
-            res.status(200).json(result);
+
+
+            res.status(200).json({ name: result.name });
+
+
+
+        } else if (req.method === 'GET') {
+            const obj = { msg: 'oieee' }
+            res.status(200).json(obj)
         } else {
-            res.status(500).json(new Date().getDate())
+            res.status(500).json({ data: new Date().getDate(), msg: "ops" })
         }
     } catch (error) {
         console.log(error)
     }
 }
 
-const getUserById = async (userId) =>{
+const getUserById = async (userId) => {
+
+
+    let firebase = loadFirebase();
 
     const userRef = firebase.firestore().collection('users');
     const query = await userRef.doc(userId).get();
@@ -28,7 +38,7 @@ const getUserById = async (userId) =>{
         return [];
     }
 
-    return query
+    return query.data()
 
 }
 
